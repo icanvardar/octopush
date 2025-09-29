3a3fb1aefc030d86793c851175fbfb8ab38ff7a5
Can Vardar <ismailcanvardar@gmail.com>
2025-09-29 16:34:23 +0300

    chore: implement autocompletion and output features

--- src/util/cli.rs
+++ src/util/cli.rs
@@ -1,6 +1,10 @@
+use crate::util::path_completer::dialoguer_path_input;
 use crate::{
     core::{app::App, auth::AuthType, profile::Profile},
-    util::system::cwd,
+    util::{
+        output::{OperationType, Runner},
+        system::cwd,
+    },
 };
 use clap::{Parser, Subcommand};
 use dialoguer::{Input, Select};
@@ -23,7 +27,7 @@ enum Command {
         email: Option<String>,
         #[arg(short, long)]
         auth_type: Option<AuthType>,
-        #[arg(short, long)]
+        #[arg(short('g'), long)]
         hostname: Option<String>,
         #[arg(short, long)]
         ssh_key_path: Option<String>,
@@ -41,8 +45,14 @@ enum Command {
     ResetProfile,
 }
 
+// NOTE:
+// no whitespaces in the profile name are allowed
+// validate email in the email field
+// make the ssh_key_path input autocompletable
+
 pub fn run() -> Result<(), std::io::Error> {
     let cli = Cli::parse();
+    let runner = Runner::new();
 
     match cli.command {
         Command::AddProfile {
@@ -98,12 +108,12 @@ pub fn run() -> Result<(), std::io::Error> {
             match auth_type {
                 AuthType::None => {}
                 AuthType::SSH => {
-                    ssh_key_path = Some(
-                        Input::new()
-                            .with_prompt("Enter the path of your ssh key")
-                            .interact_text()
-                            .unwrap(),
-                    );
+                    let input = dialoguer_path_input("Enter the path of your ssh key: ");
+                    ssh_key_path = if input.trim().is_empty() {
+                        None
+                    } else {
+                        Some(input)
+                    };
                 }
                 AuthType::GH => {
                     hostname = Some(
@@ -115,52 +125,105 @@ pub fn run() -> Result<(), std::io::Error> {
                 }
             }
 
-            let profile = Profile::build(name, email, auth_type, hostname, ssh_key_path);
+            let profile = Profile::build(
+                profile_name.clone(),
+                name,
+                email,
+                auth_type,
+                hostname,
+                ssh_key_path,
+            );
 
-            App::add_profile(profile_name, profile)?;
+            let _ = runner.run(
+                || {
+                    App::add_profile(profile_name.clone(), profile)?;
 
-            // TODO: std out meaningful message
+                    Ok(())
+                },
+                OperationType::AddProfile {
+                    profile_name: profile_name.clone(),
+                },
+            );
 
             Ok(())
         }
         Command::DeleteProfile { profile_name } => {
-            App::delete_profile(profile_name)?;
+            let _ = runner.run(
+                || {
+                    App::delete_profile(profile_name.clone())?;
 
-            // TODO: std out meaningful message
+                    Ok(())
+                },
+                OperationType::DeleteProfile {
+                    profile_name: profile_name.clone(),
+                },
+            );
 
             Ok(())
         }
         Command::ListProfiles => {
-            let profiles = App::list_profiles()?;
+            let _ = runner.run(
+                || {
+                    let profiles = App::list_profiles()?;
 
-            // TODO: find a way to prettify the profiles output
+                    println!("{:?}", profiles);
+
+                    Ok(())
+                },
+                OperationType::ListProfiles,
+            );
 
             Ok(())
         }
         Command::UseProfile { profile_name } => {
             let cwd = cwd()?;
 
-            App::use_profile(profile_name, cwd)?;
+            let _ = runner.run(
+                || {
+                    let _ = App::use_profile(profile_name.clone(), cwd)?;
 
-            // TODO: std out meaningful message
+                    Ok(())
+                },
+                OperationType::UseProfile {
+                    profile_name: profile_name.clone(),
+                },
+            );
 
             Ok(())
         }
         Command::GetProfile => {
             let cwd = cwd()?;
 
-            let profile = App::get_project_profile(cwd)?;
+            let _ = runner.run(
+                || {
+                    let (profile, repo_name) = App::get_project_profile(cwd)?;
 
-            // TODO: std out meaningful message
+                    runner.message(
+                        format!(
+                            "The repository '{}' is associated with profile {}.",
+                            repo_name, profile.id
+                        )
+                        .as_str(),
+                    );
+
+                    Ok(())
+                },
+                OperationType::GetProfile,
+            );
 
             Ok(())
         }
         Command::ResetProfile => {
             let cwd = cwd()?;
 
-            App::reset_profile_for_project(cwd)?;
+            let _ = runner.run(
+                || {
+                    App::reset_profile_for_project(cwd)?;
 
-            // TODO: std out meaningful message
+                    Ok(())
+                },
+                OperationType::ResetProfile,
+            );
 
             Ok(())
         }
